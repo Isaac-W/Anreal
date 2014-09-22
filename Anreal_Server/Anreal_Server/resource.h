dabdc888e11450923c752e0712f3fbd295a19464
Isaac-W <wage.ink@gmail.com>
2014-09-21 17:03:13 -0700

    Add profile config dialog layout

--- Anreal_Server/Anreal_Server/resource.h
+++ Anreal_Server/Anreal_Server/resource.h
@@ -11,11 +11,36 @@
 #define IDC_TITLE                       1000
 #define IDC_PROFILELIST                 1002
 #define IDC_MANAGE                      1006
+#define IDC_MANAGEPROFILE               1006
 #define IDC_PROFILE_PATH                1007
+#define IDC_NEWPROFILE                  1007
 #define IDC_BROWSE_PROFILES             1008
+#define IDC_DELETEPROFILE               1008
 #define IDC_TRACKER_PORT                1009
 #define IDC_CAPTURE_PORT                1010
 #define IDC_COMPRESSION_LVL             1011
+#define IDC_START                       1011
+#define IDC_EDIT1                       1012
+#define IDC_EDIT2                       1013
+#define IDC_EDIT3                       1014
+#define IDC_EDIT4                       1015
+#define IDC_EDIT5                       1016
+#define IDC_EDIT6                       1017
+#define IDC_CHECK1                      1018
+#define IDC_EDIT7                       1019
+#define IDC_EDIT8                       1020
+#define IDC_EDIT9                       1021
+#define IDC_EDIT10                      1022
+#define IDC_EDIT11                      1023
+#define IDC_CHECK2                      1024
+#define IDC_EDIT12                      1025
+#define IDC_EDIT13                      1026
+#define IDC_EDIT14                      1027
+#define IDC_EDIT15                      1028
+#define IDC_EDIT16                      1029
+#define IDC_CHECK3                      1030
+#define IDC_EDIT17                      1031
+#define IDC_EDIT18                      1032
 
 // Next default values for new objects
 // 
@@ -23,7 +48,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        203
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
